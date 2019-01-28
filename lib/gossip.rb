79abc1806bca0ce56db5a00362529ab7e73d5e37
Marie <marie.goudon@laposte.net>
2019-01-28 18:13:24 +0100

    ajout de 4 lignes

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,7 +1,13 @@
 class Gossip
+ 
+  def initialize(author, content)
+    @author = author
+    @content
+  end
 
   def save
     CSV.open("./db/gossip.csv", "ab")
       csv << ["Mon super auteur", "Ma super lecture"]
   end
+
 end
