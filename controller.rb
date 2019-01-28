bbd50db9727eb4308e18d389a4e57969deaa77c4
Marie <marie.goudon@laposte.net>
2019-01-28 17:04:26 +0100

    ajout de trois lignes

--- controller.rb
+++ controller.rb
@@ -14,4 +14,6 @@ end
 #Nous allons expliquer ces lignes de code, mais d'abord exécute l'application controller.rb après avoir fait l'installation du serveur. Si tu vas sur http://localhost:4567/
 
 
-
+post '/gossips/new/' do 
+  puts "Ce programme ne fait rien pour le moment, on va donc afficher un message en console"
+end
