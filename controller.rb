b4dd22318ed4cd926b4e0de02cae6bb36f2c0efe
Marie <marie.goudon@laposte.net>
2019-01-28 16:48:20 +0100

    ajout dune ligne

--- controller.rb
+++ controller.rb
@@ -5,10 +5,13 @@ Bundler.require
 
 class ApplicationController < Sinatra::Base
   get '/' do
-    "<html><head><title>The Gossip Project</title></head><body><h1>Mon super site de gossip !</h1></body></html>"
+    erb :index
   end
 
   run! if app_file == $0
 end
 
 #Nous allons expliquer ces lignes de code, mais d'abord exécute l'application controller.rb après avoir fait l'installation du serveur. Si tu vas sur http://localhost:4567/
+
+
+
